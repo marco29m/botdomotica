91e43d2d061f6180c018a180d00ef02fdda9ed6e
marco29m <77853197+marco29m@users.noreply.github.com>
2021-01-30 12:10:58 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,116 +1,92 @@
-from pygame import mixer
+#Importamos bibliotecas
 import time, datetime
 import RPi.GPIO as GPIO
 import telepot
 from telepot.loop import MessageLoop
-green = 19
-yellow = 6
-red = 13
-blue = 5
-
+#Declaramos Variables
+blue = 19
+yellow = 6 
+red = 13 
+green = 5
+#Inicializamos los pines en nivel bajo
 now = datetime.datetime.now()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-##LED Blue
 GPIO.setup(blue, GPIO.OUT)
-GPIO.output(blue, 0) #Off initially
-#LED Yellow
+GPIO.output(blue, 0)
+
 GPIO.setup(yellow, GPIO.OUT)
-GPIO.output(yellow, 0) #Off initially
-#LED Red
+GPIO.output(yellow,0)
+
 GPIO.setup(red, GPIO.OUT)
-GPIO.output(red, 0) #Off initially
-#LED green
+GPIO.output(red, 0)
+
 GPIO.setup(green, GPIO.OUT)
-GPIO.output(green, 0) #Off initially
-0
+GPIO.output(green, 0)
+#Definimos una funcion
+
 def action(msg):
-	chat_id = msg['chat']['id']
-	command = msg['text']
+    chat_id = msg['chat']['id']
+    command = msg['text']
 
-	print ('Received: %s' % command)
-	message = "Comandos:"
-	telegram_bot.sendMessage (chat_id, message)
-	message = "all on \n all off \n sala on \n patio on \n cocina on \n comedor on \n sala off \n patio off \n cocina off \n comedor off "
-	telegram_bot.sendMessage (chat_id, message)
-	if 'on' in command:
-		message = "on"
-		if 'sala' in command:
-			message = message + " Sala "
-			GPIO.output(blue, 1)
-		if 'comedor' in command:
-			message = message + " Comedor "
-			GPIO.output(yellow, 1)
-		if 'patio' in command:
-			message = message + " Patio "
-			GPIO.output(red, 1)
-		if 'cocina' in command:
-			message = message + " Cocina "
-			GPIO.output(green , 1)
-		if 'all' in command:
-			message = message + "all "
-			GPIO.output(blue, 1)
-			GPIO.output(yellow, 1)
-			GPIO.output(red, 1)
-			GPIO.output(green, 1)
-		message = message + " lights "
-		telegram_bot.sendMessage (chat_id, message)
+    print('Received: %s' % command)
+    message = "Comandos: "
+    telegram_bot.sendMessage (chat_id, message)
+    message = "all on \n all off \n sala on \n patio on \n cocina on \n comedor on \n sala off \n patio off \n cocina off \n comedor off \n"
+    telegram_bot.sendMessage (chat_id, message)
+    if 'on' in command:
+        message = 'on' 
+        if 'sala' in command:
+            message = message + "sala"
+            GPIO.output(blue, 1)
+        if 'comedor' in command:
+            message = message + "comedor"
+            GPIO.output(yellow, 1)
+        if 'patio' in command:
+            message = message + "patio"
+            GPIO.output(red, 1)
+        if 'cocina' in command:
+            message = message + "cocina"
+            GPIO.output(green, 1)
+        if 'all' in command:
+            message =  message + "all"
+            GPIO.output(blue, 1)   
+            GPIO.output(yellow, 1)
+            GPIO.otput(red, 1) 
+            GPIO.output(green, 1)             
 
-	if 'off' in command:
-		message = "off"
-		if 'sala' in command:
-			message = message + " Sala "
-			GPIO.output(blue, 0)
-		if 'comedor' in command:
-			message = message + " Comedor "
-			GPIO.output(yellow, 0)
-		if 'patio' in command:
-			message = message + " Patio "
-			GPIO.output(red, 0)
-		if 'cocina' in command:
-			message = message + " Cocina "
-			GPIO.output(green , 0)
-		if 'all' in command:
-			message = message + " all "
-			GPIO.output(blue, 0)
-			GPIO.output(yellow, 0)
-			GPIO.output(red, 0)
-			GPIO.output(green, 0)
-	if 'music' in command :
-		message = "Abriendo reproductor"
-		telegram_bot.sendMessage (chat_id, message)
-		message = "Seleciona una cancion"
-		telegram_bot.sendMessage (chat_id, message)
-		message = "1-Exile-Taylor Swift"
-		telegram_bot.sendMessage (chat_id, message)
-		message = message + "lights"
-		telegram_bot.sendMessage (chat_id, message)
-	if '1' in command :
-		mixer.init()
-		mixer.music.load('/home/pi/exile.mp3')
-		mixer.music.set_volume(0.7)
-		mixer.music.play()
-		message = "p para pausar"
-		telegram_bot.sendMessage (chat_id, message)
-		message = "r para reanudar"
-		telegram_bot.sendMessage (chat_id, message)
-		message = "s para detener"
-		telegram_bot.sendMessage (chat_id, message)
-		message = "e elegir nueva"
-		telegram_bot.sendMessage (chat_id, message)
-	if 'p' in command :
-		mixer.music.pause()
-	if 'r' in command :
-		mixer.music.unpause()
-	if 's' in command :
-		mixer.music.stop
+    message = message + "light(s)"
+    telegram_bot.sendMessage(chat_id, message)
 
-telegram_bot = telepot.Bot('botkey')
-print (telegram_bot.getMe())
+    if 'off' in command:
+        message = 'off' 
+        if 'sala' in command:
+            message = message + "sala"
+            GPIO.output(blue, 0)
+        if 'comedor' in command:
+            message = message + "comedor"
+            GPIO.output(yellow, 0)
+        if 'patio' in command:
+            message = message + "patio"
+            GPIO.otput(red, 0)
+        if 'cocina' in command:
+            message = message + "cocina"
+            GPIO.output(green, 0)
+        if 'all' in command:
+            message =  message + "all"
+            GPIO.output(blue, 0)   
+            GPIO.output(yellow, 0)
+            GPIO.otput(red, 0) 
+            GPIO.output(green, 0)             
+
+    message = message + "light(s)"
+    telegram_bot.sendMessage(chat_id, message)
+
+telegram_bot = telepot.Bot('1576472367:AAEGUfU1bBOtw_JN7v1LNBkKM67UsZbUkKE')   
+print(telegram_bot.getMe())
 
 MessageLoop(telegram_bot, action).run_as_thread()
 print ('Up and Running...')
-
 while 1:
-	time.sleep(10)
+    time.sleep(10)
